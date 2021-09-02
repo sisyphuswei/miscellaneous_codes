d26596141f1cbaee46c5dc188c8502c3d607c96e
Xiaoqi Wei <40246590+weixiaoqimath@users.noreply.github.com>
2021-09-01 20:07:07 -0400

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,8 +1,8 @@
-def matrix_writer(array, filename = 'test'):
+def matrix_writer(array, filename = 'test.txt'):
     """array is a numpy array or list of lists. We write it into a txt file.
     filename is the name of the txt file.
     """
-    with open("{}.txt".format(filename), 'w') as f:
+    with open(filename, 'w') as f:
         for row in array:
             for i, entry in enumerate(row):
                 f.write("{} ".format(entry))
@@ -14,7 +14,16 @@ def matrix_reader(filename = 'test.txt'):
     array_list = []
     for line in text:
         array_list.append(list(map(float, line.split())))
-    return np.array(array_list) 
+    text.close()
+    return np.array(array_list)  
+
+def array_reader(filename = 'test.txt'):
+    text = open(filename, "r")
+    array_list = []
+    for line in text:
+        array_list.append(list(map(float, line.split())))
+    text.close()
+    return array_list
     
 def xyz_parser(filename):
     text = open(filename, "r")
@@ -22,6 +31,7 @@ def xyz_parser(filename):
     atom_list = []
     for line in text:
         atom_list.append(list(map(float, line.split()[:3])))
+    text.close()
     return np.array(atom_list)
    
 def laplacian_parser(filename):
@@ -36,4 +46,5 @@ def laplacian_parser(filename):
         else:
             matrices.append(np.array(mat))
             mat = []
+    text.close()
     return matrices
